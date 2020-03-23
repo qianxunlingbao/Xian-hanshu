177aaa72dee0f945729f1cafb1f6883203349d8d
liubin <1433917529@qq.com>
2020-03-23 22:29:37 +0800

    javascript语言排名变化

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function(){
+   
+   // 基于准备好的dom，初始化echarts实例
+   var myChart = echarts.init(document.getElementById('main'));
+    
+   // 指定图表的配置项和数据
+   var option = {
+     title: {
+                text: 'JavaScript语言排名变化'
+            },
+            tooltip: {},
+            legend: {
+                data:['排名']
+            },
+            xAxis: {
+                data: ["2000","2005","2010","2015","2010"]
+            },
+            yAxis: {},
+            series: [{
+                name: '排名',
+                type: 'line',
+                data: [6,9,8,8,7]
+            }]  
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+ 
+});
